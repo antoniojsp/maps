4d48d5d52445db5d9a4a93210c7b514fea1c09e9
antonio Silva <>
2021-07-07 16:51:41 -0700

    ultyimo

--- maps.py
+++ maps.py
@@ -10,35 +10,43 @@ config = configparser.ConfigParser()
 config.read("credentials.ini")
 API_KEY = config["DEFAULT"]["KEY_FLASH"]
 
+def max_min(data:list, attr:str):
+    '''
+    (list, str) -> (number, number)
+
+    Gets a dictionary and a key. Then use that key to loop up values and return the minumum and maximum values.
+    '''
+    sortie = sorted(data, key=lambda x: x[attr], reverse=True)
+    max = sortie[0][attr]
+    min = sortie[-1][attr]
+
+    return min, max
+
 def plot_route(file:str, color:str, attribute:str, output_name:str):
     cmap = matplotlib.cm.get_cmap(color)
 
     # Create the map plotter:
     apikey = API_KEY # (your API key here)
-    gmap = gmplot.GoogleMapPlotter(44.0521, -123.0868, 14, apikey=apikey)
-
     data = get_info_formatted(file)
 
+    #middle point to center the map
+    lat_min, lat_max = max_min(data, "latitude")
+    lon_min, lon_max = max_min(data, "longitude")
+    middle_point_lat = (lat_min+lat_max)/2
+    middle_point_lon= (lon_min+lon_max)/2
 
-    distance = [i[attribute] for i in data]
+    gmap = gmplot.GoogleMapPlotter(middle_point_lat, middle_point_lon, 14, apikey=apikey)
 
-
-
-
-    sortie = sorted(data, key=lambda x: x[attribute], reverse=True)
-
-    max = sortie[1][attribute]
-    min = sortie[-1][attribute]
-
+    min, max = max_min(data, attribute)
+    # print(min, max)
     for i in range(len(data)-1):
         path1 = zip(*[ (data[i]["latitude"],data[i]["longitude"]),
                        (data[i+1]["latitude"],data[i+1]["longitude"])
                      ])
 
         normalize = (data[i][attribute]-min)/(max-min) #select color
-        rgba = cmap(normalize)
-        gmap.plot(*path1, edge_width=5, color=matplotlib.colors.rgb2hex(rgba))
-        i+=1
+        rgba = cmap(normalize) # pass to cmap 
+        gmap.plot(*path1, edge_width=5, color=matplotlib.colors.rgb2hex(rgba)) # plot 
 
     # Draw the map:
     gmap.draw(output_name)
--- tcx_parser.py
+++ tcx_parser.py
@@ -23,6 +23,32 @@ def append_to_end(list_dictionary:list, key:str, item):
     list_dictionary[-1] = dict(temp)
     return list_dictionary
 
+def add_average(lista, rango):
+    promedio = []
+    suma = 0
+    avg = len(lista)%rango
+    result = []
+
+    for i, j in enumerate(lista):
+        suma+=j
+        if (i+1)%rango == 0 and i !=0:
+            promedio.append(suma/rango)
+            suma=0
+        elif i == len(lista)-1:
+            promedio.append(suma/avg)
+
+    elements_num = int(len(lista)/rango)
+
+    for i in promedio[:elements_num]:
+        for j in range(rango):
+            result.append(i)
+
+    for i in promedio[elements_num:]:
+        for j in range(avg):
+            result.append(i)
+    
+    return result
+
 
 def get_info_formatted(tcx_file:str):
     '''
@@ -41,23 +67,31 @@ def get_info_formatted(tcx_file:str):
 
     location_data = []
     distancia_for_now = 0
+    temp_speed = []
+
+    for i in result:
+        dis = re.search(distance, str(i))
+        if dis != None: #if distance present, add to the last dict
+            variation = abs(float(dis.group(1)) - distancia_for_now)
+            distancia_for_now = float(dis.group(1))
+            speed = (variation*18)/float(5)
+            temp_speed.append(speed)
+
+    velo = add_average(temp_speed, 10)
+    k = 0
 
     for i in result:
         lat = re.search(latitude, str(i))
         lon = re.search(longitude, str(i))
         alt = re.search(altitude, str(i))
-        dis = re.search(distance, str(i))
         if lat and lon and alt != None: #if it doesn't has a latitude or longitude, skips the line
-            location_data.append({"latitude":float(lat.group(1)), "longitude":float(lon.group(1)) , "altitude": float(alt.group(1))})
-
-        if dis != None: #if distance present, add to the last dict
-            variation = abs(float(dis.group(1)) - distancia_for_now)
-            distancia_for_now = float(dis.group(1))
-            speed = (variation*18)/float(5)
-
-            append_to_end(location_data, "speed", speed)
+            location_data.append({"latitude":float(lat.group(1)), 
+            "longitude":float(lon.group(1)), 
+            "altitude": float(alt.group(1)), 
+            "speed": float(velo[k])
+            })
+            k+=1
 
     return location_data
 
-# get_info_formatted("paseo3.tcx")
-# print(*get_info_formatted("paseo3.tcx"), sep="\n")
+
